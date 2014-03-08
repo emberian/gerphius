0cdb3f472e8579c8328453e5a9283e38a78b176f
Corey Richardson <corey@octayn.net>
2014-03-07 23:18:51 -0500

    Fix tabs in vertex shader

--- vertex.glsl
+++ vertex.glsl
@@ -8,8 +8,8 @@ in ivec2 texcoord;
 out vec2 Texcoord;
 
 void main() {
-	// this gives us [0, 1]. We need [-1, 1]. Scale by two, subtract by one in each direction
-	vec2 coord = (2.0 * (position / windowsize)) - vec2(1.0);
+    // this gives us [0, 1]. We need [-1, 1]. Scale by two, subtract by one in each direction
+    vec2 coord = (2.0 * (position / windowsize)) - vec2(1.0);
     gl_Position = vec4(coord, 0.0, 1.0);
     Texcoord = vec2(texcoord);
 }

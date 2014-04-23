2f6d90399381d9217be2e606bb8d282b4320a488
Corey Richardson <corey@octayn.net>
2014-04-22 22:34:02 -0400

    rotation

--- assets/vertex.glsl
+++ assets/vertex.glsl
@@ -1,6 +1,7 @@
 #version 150
 
 uniform vec2 windowsize;
+uniform float rotation;
 
 in vec2 position;
 in vec2 texcoord;
@@ -8,6 +9,14 @@ in vec2 texcoord;
 out vec2 Texcoord;
 
 void main() {
-    gl_Position = vec4(position, 0.0, 1.0);
+    float s = sin(rotation);
+    float c = cos(rotation);
+    mat4 rzm = mat4(
+            c, s, 0, 0,
+            -s, c, 0, 0,
+            0, 0, 1, 0,
+            0, 0, 0, 1
+    );
+    gl_Position = rzm * vec4(position, 0.0, 1.0);
     Texcoord = vec2(texcoord);
 }
